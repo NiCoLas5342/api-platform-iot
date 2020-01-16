0494a41a3e7b44f63d4dc10dc5bb1f9f0cff7255
besenitescia <burak.esen@edu.itescia.fr>
2020-01-16 15:42:30 +0100

    Création des classes métiers et des crud (avant migration)

---
+++ api/src/Entity/Personne.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\PersonneRepository")
+ */
+class Personne
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Salle", inversedBy="personnes")
+     */
+    private $salle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSalle(): ?Salle
+    {
+        return $this->salle;
+    }
+
+    public function setSalle(?Salle $salle): self
+    {
+        $this->salle = $salle;
+
+        return $this;
+    }
+}
---
+++ api/src/Entity/Salle.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\SalleRepository")
+ */
+class Salle
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numeroSalle;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $etage;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Personne", mappedBy="salle")
+     */
+    private $personnes;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Batiment", inversedBy="salles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $batiment;
+
+    public function __construct()
+    {
+        $this->personnes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumeroSalle(): ?int
+    {
+        return $this->numeroSalle;
+    }
+
+    public function setNumeroSalle(int $numeroSalle): self
+    {
+        $this->numeroSalle = $numeroSalle;
+
+        return $this;
+    }
+
+    public function getEtage(): ?int
+    {
+        return $this->etage;
+    }
+
+    public function setEtage(int $etage): self
+    {
+        $this->etage = $etage;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Personne[]
+     */
+    public function getPersonnes(): Collection
+    {
+        return $this->personnes;
+    }
+
+    public function addPersonne(Personne $personne): self
+    {
+        if (!$this->personnes->contains($personne)) {
+            $this->personnes[] = $personne;
+            $personne->setSalle($this);
+        }
+
+        return $this;
+    }
+
+    public function removePersonne(Personne $personne): self
+    {
+        if ($this->personnes->contains($personne)) {
+            $this->personnes->removeElement($personne);
+            // set the owning side to null (unless already changed)
+            if ($personne->getSalle() === $this) {
+                $personne->setSalle(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getBatiment(): ?Batiment
+    {
+        return $this->batiment;
+    }
+
+    public function setBatiment(?Batiment $batiment): self
+    {
+        $this->batiment = $batiment;
+
+        return $this;
+    }
+}
